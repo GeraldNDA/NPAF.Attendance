e5bb9ca233a9d93fdbec170f7b17f7c045eaa06b
GeraldNDA <aryeeteygerald@rogers.com>
2018-01-02 15:13:09 -0500

    Revert Service Worker experimentation

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-this.ontask = function(task) {
-  alert(task.data.message);
-  console.log("Task scheduled at: " + new Date(task.time));
-  // From here on we can write the data to IndexedDB, send it to any open windows,
-  // display a notification, etc.
-}
-
